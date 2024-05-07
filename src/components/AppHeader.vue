36a98ffa0c3e43bc5910fd8c380645a18b201842
sundevildi <sundevildi@gmail.com>
2024-05-07 10:26:03 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "AppHeader"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
